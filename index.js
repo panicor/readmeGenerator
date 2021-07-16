d603b90988fd8c69f39ca07b17fe0d9b5085b413
panicor <rachelpanico@hotmail.com>
2021-07-16 17:50:56 -0400

    added badges

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+//requires node file system
 const fs = require("fs");
 const path = require("path");
+//requires node inquirer
 const inquirer = require("inquirer");
+//loads generateMarkdown file in utils
 const generateMarkdown = require("./utils/generateMarkdown")
 
-// TODO: Create an array of questions for user input
+//array of questions
 const questions = [
 {
     type: "input",
@@ -41,6 +44,13 @@ const questions = [
     message: "Please enter any contribution guidelines."
 },
 {
+    type: "list",
+    name: "license",
+    message: "Please select the the project license.",
+    //choices separated by line
+    choices: ["MIT", new inquirer.Separator(), "BSD", new inquirer.Separator(), "Apache", new inquirer.Separator(), "GPL"]
+},
+{
     type: "input",
     name: "test",
     message: "Please enter any test instructions."
@@ -49,16 +59,16 @@ const questions = [
 ];
 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
 function init() {
+    //prints answer object
     inquirer.prompt(questions).then(answers => {
         console.log('ANSWERS OBJECT -> ', answers)
+        //writes new readme file with object as content
        const content = generateMarkdown(answers)
        fs.writeFile("README.md", content, err =>{
-           if (err){
+           //throws error if error
+        if (err){
                console.error(err);
                return
            }
@@ -71,37 +81,3 @@ function init() {
 
 // Function call to initialize app
 init();
-
-
-//  application that accepts user input
-//  - prompted for information about my application repository
-//  - title of my project
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-
-
-        //- Description, 
-        //-Table of Contents,
-        // -  Installation, 
-        // -Usage, 
-        // - License, 
-        // -Contributing, 
-        // -Tests,
-        // and Questions
-
-
-
-
-
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,76 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = ``;
+
+  switch(license){
+    case "MIT":
+      badge = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+      return badge;
+    case "BSD":
+      badge = "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
+    case "GPL":
+    badge = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+    return badge;
+    case "Apache":
+    badge = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+    return badge;
+    default: break;
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link = "";
+switch(license){
+  case "MIT":
+    link = `[${license}](https://opensource.org/licenses/MIT)`;
+    return link;
+  case "BSD":
+    link = `[${license}](https://opensource.org/licenses/BSD-3-Clause)`;
+    return link;
+  case "GPL":
+    link = `[${license}](https://www.gnu.org/licenses/gpl-3.0)`;
+    return link;
+  case "Apache": 
+    link = `[${license}](https://opensource.org/licenses/Apache-2.0)`;
+    return link;
+    default: break;
+}
+
+
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license){
+ return `This project is licensed under a ${license} license.`;
+  }
+  return "";
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
+
   return `
   # ${answers.title}
+
+  ${renderLicenseBadge(answers.license)}
+
+  ## Table of Contents
+- [Description](#description)
+- [Usage Information](#usage-information)
+- [Installation Instructions](#installation-instructions)
+- [Contribution Guidelines](#contribution-guidelines)
+- [License](#license)
+- [Test Instructions](#test-instructions)
+- [Questions](#questions)
+
   ## Description
   ${answers.description}
   ## Usage Information
@@ -22,10 +79,13 @@ function generateMarkdown(answers) {
   ${answers.installation}
   ## Contribution Guidelines
   ${answers.contributionGuidelines}
+  ## License
+  ${renderLicenseLink(answers.license)}\n
+  ${renderLicenseSection(answers.license)}
   ## Test Instructions
   ${answers.test}
   ## Questions
-  ${answers.github}
+  [GitHub: ${answers.github}](https://github.com/${answers.github})\n
   ${answers.email}
 `;
 }
